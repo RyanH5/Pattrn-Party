1747d0b6b8860b9c567420f2a51ac802360a9ea8
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-09-06 08:19:52 -0600

    Add flex cards

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ function toggleTab(event) {
   $(event.target).removeClass('unselected');
   [...$(event.target).siblings('li').removeClass('selected')];
   [...$(event.target).siblings('li').addClass('unselected')];
+  // toggleDisplay();
 }
+
+// function toggleDisplay() {
+//   $('.image--content-container').removeClass('active');
+//   $('.image--content-container').addClass('inactive');
+
+//   const contentDisplays = [...$(event.target).parents('ul').siblings('.image--content-container')];
+//   console.log(event.target.id)
+//   contentDisplays.forEach(display => {
+//     const id = display.id.split('content')
+//     const index = id[1];
+//   })
+//  Find the image--content-container w/ same number in id as event.target and add active class / remove inactive class
+
+// }
